2b5177574e88d039f60d5e3b343d597bfd2dabda
sia <aaay8228@gmail.com>
2024-11-23 18:47:33 +0900

    feat: sia/section04 연속된 자연수의 합 update

---
+++ sia/section04/SequentialSumFinder.java
@@ -0,0 +1,33 @@
+package section04;
+
+import java.util.Scanner;
+
+public class SequentialSumFinder {
+
+	public static void main(String[] args) {
+		Scanner scanner = new Scanner(System.in);
+		int n = scanner.nextInt();
+
+		System.out.println(solution(n));
+	}
+
+	private static int solution(int n) {
+		int answer = 0;
+
+		int end = n - 1; // 자기자신은 제외
+		int sum = 0;
+
+		for (int i = end; i > 0; i--) {
+			sum += i;
+
+			while (sum > n) {
+				sum -= end--;
+			}
+
+			if (sum == n) {
+				answer++;
+			}
+		}
+		return answer;
+	}
+}
