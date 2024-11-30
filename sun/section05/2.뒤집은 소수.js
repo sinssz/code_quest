59f2c0c886313fcf4c63d3ec20fc55b1dca06d38
kks <rudtjs9956@gmail.com>
2024-11-30 22:39:57 +0900

    feat: section05-02

--- sun/section05/2.뒤집은 소수.js
+++ sun/section05/2.뒤집은 소수.js
@@ -8,25 +8,11 @@ const solution = (arr) => {
         return Number(reversed); // 뒤집은 값을 숫자로 변환
     }
 
-    // 숫자가 소수인지 확인하는 함수
     function isPrime(number) {
-        // 1 이하의 숫자는 소수가 아님
-        if (number <= 1) {
-            return false;
-        }
+        if (number <= 1) return false;
 
-        // 2와 3은 소수
-        if (number <= 3) {
-            return true;
-        }
-
-        // 짝수와 3의 배수 제외
-        if (number % 2 === 0 || number % 3 === 0) {
-            return false;
-        }
-
-        for (let i = 5; i * i <= number; i += 6) {
-            if (number % i === 0 || number % (i + 2) === 0) {
+        for (let i = 2; i <= Math.sqrt(number); i++) {
+            if (number % i === 0) {
                 return false;
             }
         }
