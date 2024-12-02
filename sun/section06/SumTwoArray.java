6120385ad3d626f70578a5f427add66ae727ccb4
kks <rudtjs9956@gmail.com>
2024-12-02 14:57:52 +0900

    refactor: section06-01

--- sun/section06/SumTwoArray.java
+++ sun/section06/SumTwoArray.java
@@ -1,7 +1,6 @@
 package section06;
 
 import java.util.Arrays;
-import java.util.stream.IntStream;
 
 // 두 배열 합치기
 public class SumTwoArray {
@@ -13,9 +12,36 @@ public class SumTwoArray {
 		System.out.println(Arrays.toString(solution(arr1, arr2)));
 	}
 
-	private static int[] solution(int[] arr1, int[] arr2) {
-		return IntStream.concat(IntStream.of(arr1), IntStream.of(arr2))
-			.sorted()
-			.toArray();
+	// private static int[] solution(int[] arr1, int[] arr2) {
+	// 	return IntStream.concat(IntStream.of(arr1), IntStream.of(arr2))
+	// 		.sorted()
+	// 		.toArray();
+	// }
+
+	// 투 포인터 적용
+	public static int[] solution(int[] arr1, int[] arr2) {
+		int n = arr1.length;
+		int m = arr2.length;
+		int[] result = new int[n + m];
+
+		int i = 0, j = 0, k = 0;
+
+		while (i < n && j < m) {
+			if (arr1[i] <= arr2[j]) {
+				result[k++] = arr1[i++];
+			} else {
+				result[k++] = arr2[j++];
+			}
+		}
+
+		while (i < n) {
+			result[k++] = arr1[i++];
+		}
+
+		while (j < m) {
+			result[k++] = arr2[j++];
+		}
+
+		return result;
 	}
 }
