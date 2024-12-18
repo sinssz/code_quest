576ca4f5628b0e165e7439620c3708c43ef30a19
kks <rudtjs9956@gmail.com>
2024-12-18 11:15:06 +0900

    refactor: section08-03

--- sun/section08/SpecialSort.java
+++ sun/section08/SpecialSort.java
@@ -13,26 +13,42 @@ public class SpecialSort {
 		int n = input.length;
 
 		for (int i = 0; i < n - 1; i++) {
-			if (input[i] < 0) {
-				continue;
-			}
-
-			for (int j = i; j < n; j++) {
-				if (input[j] > 0) {
-					continue;
+			for (int j = 0; j < n - i - 1; j++) {
+				if (input[j] > 0 && input[j + 1] < 0) {
+					int temp = input[j];
+					input[j] = input[j + 1];
+					input[j + 1] = temp;
 				}
-
-				int selected = input[j];
-
-				for (int k = j; k > i; k--) {
-					input[k] = input[k - 1];
-				}
-
-				input[i] = selected;
-				break;
 			}
 		}
 
 		return input;
 	}
+
+	// private static int[] specialSort(int[] input) {
+	// 	int n = input.length;
+	//
+	// 	for (int i = 0; i < n - 1; i++) {
+	// 		if (input[i] < 0) {
+	// 			continue;
+	// 		}
+	//
+	// 		for (int j = i; j < n; j++) {
+	// 			if (input[j] > 0) {
+	// 				continue;
+	// 			}
+	//
+	// 			int selected = input[j];
+	//
+	// 			for (int k = j; k > i; k--) {
+	// 				input[k] = input[k - 1];
+	// 			}
+	//
+	// 			input[i] = selected;
+	// 			break;
+	// 		}
+	// 	}
+	//
+	// 	return input;
+	// }
 }
