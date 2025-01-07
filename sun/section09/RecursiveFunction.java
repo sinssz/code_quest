b0db93690dec59a6901365baab7781d9f1bd7c6c
kks <rudtjs9956@gmail.com>
2025-01-07 16:54:15 +0900

    feat: section09-01

---
+++ sun/section09/RecursiveFunction.java
@@ -0,0 +1,16 @@
+package section09;
+
+public class RecursiveFunction {
+
+    public static void main(String[] args) {
+        dfs(3);
+    }
+
+    private static void dfs(int n) {
+        if (n == 0) {
+            return;
+        }
+        dfs(n - 1);
+        System.out.println(n);
+    }
+}
