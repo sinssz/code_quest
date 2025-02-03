dadaf78ab3cfa00f741e4ea1796841903d84cdd0
kks <rudtjs9956@gmail.com>
2025-02-03 10:55:10 +0900

    feat: 백준 11720

---
+++ sun/BaekJoon11720.java
@@ -0,0 +1,19 @@
+import java.util.Scanner;
+
+public class BaekJoon11720 {
+
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int N = sc.nextInt();
+        String s = sc.next();
+
+        int sum = 0;
+        char[] chars = s.toCharArray();
+
+        for (char c : chars) {
+            sum += Character.getNumericValue(c);
+        }
+
+        System.out.println(sum);
+    }
+}
