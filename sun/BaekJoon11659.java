6bc5aeffa4afa3b25821bb1ef45f617b965c9e87
kks <rudtjs9956@gmail.com>
2025-02-03 22:18:28 +0900

    feat: 구간합 문제

---
+++ sun/BaekJoon11659.java
@@ -0,0 +1,33 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
+
+public class BaekJoon11659 {
+
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st = new StringTokenizer(br.readLine());
+
+        int N = Integer.parseInt(st.nextToken());
+        int M = Integer.parseInt(st.nextToken());
+
+        int[] prefixSum = new int[N + 1];
+
+        st = new StringTokenizer(br.readLine());
+        for (int i = 1; i <= N; i++) {
+            prefixSum[i] = prefixSum[i - 1] + Integer.parseInt(st.nextToken());
+        }
+
+        StringBuilder sb = new StringBuilder();
+        for (int q = 0; q < M; q++) {
+            st = new StringTokenizer(br.readLine());
+            int i = Integer.parseInt(st.nextToken());
+            int j = Integer.parseInt(st.nextToken());
+
+            sb.append(prefixSum[j] - prefixSum[i - 1]).append("\n");
+        }
+
+        System.out.println(sb);
+    }
+}
