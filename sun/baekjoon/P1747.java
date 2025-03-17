ad1833b971ff6d1a7a699d0bb577e34685f6975f
kks <rudtjs9956@gmail.com>
2025-03-18 00:02:13 +0900

    feat: 07-1 소수 구하기

--- sun/baekjoon/P1747.java
+++ sun/baekjoon/P1747.java
@@ -8,30 +8,36 @@ public class P1747 {
         Scanner sc = new Scanner(System.in);
         int N = sc.nextInt();
 
-        boolean[] isPrime = new boolean[1000001];
-
-        // 1. 소수 판별 (에라토스테네스의 체)
-        for (int i = 2; i <= 1000000; i++) {
-            isPrime[i] = true;
-        }
-
-        for (int i = 2; i * i <= 1000000; i++) {
-            if (isPrime[i]) {
-                for (int j = i * i; j <= 1000000; j += i) {
-                    isPrime[j] = false;
-                }
-            }
-        }
-
-        // 2. N 이상이면서 가장 작은 팰린드롬 소수 찾기
         int i = N;
         while (true) {
-            if (isPrime[i] && isPalindrome(i)) {
+            if (isPrime(i) && isPalindrome(i)) {
                 System.out.println(i);
                 break;
             }
             i++;
         }
+    }
+
+    private static boolean isPrime(int number) {
+        if (number < 2) {
+            return false;
+        }
+
+        if (number == 2 || number == 3) {
+            return true;
+        }
+
+        if (number % 2 == 0 || number % 3 == 0) {
+            return false;
+        }
+
+        for (int i = 5; i * i <= number; i++) {
+            if (number % i == 0) {
+                return false;
+            }
+        }
+
+        return true;
     }
 
     private static boolean isPalindrome(int num) {
